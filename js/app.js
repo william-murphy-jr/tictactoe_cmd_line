f8521036192255807d871e1ba6d9b96985258c98
Bill Murphy <bill559m@gmail.com>
2019-02-06 13:43:23 -0800

    initial commit of empty game

---
+++ js/app.js
@@ -0,0 +1,16 @@
+'use strict';
+console.log('Tic Tac Toe :)');
+
+const tictactoe = () => {
+  const winCombos =
+    var winCombos = [
+      [0, 1, 2],
+      [0, 4, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [2, 4, 6],
+      [3, 4, 5],
+      [6, 7, 8]
+    ];
+}
